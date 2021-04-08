8c26c3c46594ca56f1fc1735338be785ac191bd1
tavo <gasb_150@hotmail.com>
2021-04-08 18:59:42 -0500

    Restore filesQQ

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// import load from './load'
+
+
+import getInfo from './api'
+
+const loadPage = (() => {
+    async function load() {
+        const cityName = document.getElementById('cityName')
+        const countryName = document.getElementById('countryName')
+        const temp = document.getElementById('tempAct')
+        const tempFeel = document.getElementById('tempFeel')
+        const humidity = document.getElementById('humidity')
+        const desc = document.getElementById('desc')
+
+        const data = await getInfo().then((response => {
+            cityName.innerHTML = response.name
+            countryName.innerHTML = response.sys.country
+            temp.innerHTML = response.main.temp
+            tempFeel.innerHTML = response.main.feels_like
+            humidity.innerHTML = response.main.humidity
+            desc.innerHTML = response.weather[0].description
+    
+        }))
+    } return {
+        load
+    };
+})()
+
+loadPage.load()
