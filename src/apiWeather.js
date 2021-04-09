eed40a7c2c1a1afb7204cbd0e46f1596a491010b
tavo <gasb_150@hotmail.com>
2021-04-09 17:39:45 -0500

    Replace to secure api

--- src/apiWeather.js
+++ src/apiWeather.js
@@ -1,6 +1,6 @@
 export default async function getInfo (cityName="Ontario,CA") {
        const key = 'b9f735ed2467b45a05dde8fe608efd08'
-       const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${key}`
+       const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${key}`
        const response = await fetch(url, {mode: 'cors'}).then(response=> response.json())
     
         return response
