2ab5a3e4468b57d29b37699395222ff76f1ca63c
tavo <gasb_150@hotmail.com>
2021-04-09 11:02:41 -0500

    Fix location api interaction

--- src/index.js
+++ src/index.js
@@ -5,26 +5,29 @@ import getInfo from './apiWeather'
 import location from './apiLocation'
 const loadPage = (() => {
     
-    async function load(cityInput) {
+    async function load(cityInput=null) {
         const cityName = document.getElementById('cityName')
         const countryName = document.getElementById('countryName')
         const temp = document.getElementById('tempAct')
         const tempFeel = document.getElementById('tempFeel')
         const humidity = document.getElementById('humidity')
         const desc = document.getElementById('desc')
-        
+        const wind = document.getElementById('wind')
+
         const locData = await location().then((response=>{
             const cityName = response.city
             return cityName
         }))
-
+       if (cityInput ===null){
         cityInput = locData
+       }
         const data = await getInfo(cityInput).then((response => {
             cityName.innerHTML = response.name
             countryName.innerHTML = response.sys.country
-            temp.innerHTML = `${response.main.temp - 273.15}°C`
-            tempFeel.innerHTML = `${Math.round(response.main.feels_like - 273.15)}°C`
-            humidity.innerHTML = response.main.humidity
+            temp.innerHTML = `${Math.round(response.main.temp - 273.15, -2)}°C`
+            tempFeel.innerHTML = `${Math.round(response.main.feels_like - 273.15, -2)}°C`
+            wind.innerHTML = `${response.wind.speed} m/s`
+            humidity.innerHTML = `${response.main.humidity}%`
             desc.innerHTML = response.weather[0].description
     
         }
@@ -43,6 +46,8 @@ const searchBtn = document.getElementById('search')
 
 searchBtn.addEventListener("click", () =>{
     const cityInput = document.getElementById('cityInput').value
+    console.log(cityInput)
     loadPage.load(cityInput)
+    
   
 })
